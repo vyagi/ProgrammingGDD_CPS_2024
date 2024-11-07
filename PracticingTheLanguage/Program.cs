19888692a942c6f0b16910518dd6c4e999f8d2e1
vyagi <marcin.jagiela@hotmail.com>
2024-11-07 18:55:46 +0100

    Classes completed

---
+++ PracticingTheLanguage/Program.cs
@@ -0,0 +1,86 @@
+﻿var myBeautifulCup = new Cup(300);
+myBeautifulCup.Add(100);
+myBeautifulCup.Pour(70);
+Console.WriteLine(myBeautifulCup.LiquidLevel);
+
+var coffeeMachine = new CoffeeMachine("Krups 1000", 1500, 1200);
+coffeeMachine.AddGrains(700);
+coffeeMachine.AddWater(1000);
+
+coffeeMachine.MakeEspresso(myBeautifulCup);
+
+Console.WriteLine(myBeautifulCup.LiquidLevel);
+
+public class CoffeeMachine
+{
+    private readonly string _modelName;
+    private readonly int _coffeeGrainsCapacity;
+    private readonly int _waterCapacity;
+
+    private int _grainsLevel;
+    private int _waterLevel;
+    private int _espressosMade;
+
+    public CoffeeMachine(string modelName, int coffeeGrainsCapacity, int waterCapacity)
+    {
+        _modelName = modelName;
+        _coffeeGrainsCapacity = coffeeGrainsCapacity;
+        _waterCapacity = waterCapacity;
+    }
+
+    public void AddGrains(int amount)
+    {
+        _grainsLevel += amount;
+
+        if (_grainsLevel > _coffeeGrainsCapacity)
+            _grainsLevel = _coffeeGrainsCapacity;
+    }
+
+    public void AddWater(int amount)
+    {
+        _waterLevel += amount;
+
+        if (_waterLevel > _waterCapacity)
+            _waterLevel = _waterCapacity;
+    }
+
+    public void MakeEspresso(Cup cup)
+    {
+        if (_grainsLevel > 50 && _waterLevel > 300)
+        {
+            _grainsLevel -= 18;
+            _waterLevel -= 30;
+            _espressosMade++;
+            cup.Add(30);
+        }
+    }
+}
+
+public class Cup
+{
+    private readonly int _capacity;
+	private int _liquidLevel;
+
+	public Cup(int capacity)
+	{//Homework -> protect from negative value
+		_capacity = capacity;
+	}
+
+	public void Add(int amount)
+    {//Homework -> protect from negative value
+        _liquidLevel += amount;
+
+		if (_liquidLevel > _capacity)
+			_liquidLevel = _capacity;
+    }
+
+	public void Pour(int amount)
+    {//Homework -> protect from negative value
+        _liquidLevel -= amount;
+
+        if (_liquidLevel < 0)
+            _liquidLevel = 0;
+    }
+
+    public int LiquidLevel => _liquidLevel;
+}
---
+++ PracticingTheLanguage/RockScissorsPaper.cs
@@ -0,0 +1,40 @@
+﻿//Console.WriteLine("Hello in the rock, paper, scissors game");
+//Console.WriteLine("What is your name");
+//var name = Console.ReadLine();
+
+//Console.WriteLine("Choose the option:");
+//Console.WriteLine("1 - rock");
+//Console.WriteLine("2 - scissors");
+//Console.WriteLine("3 - paper");
+
+//var choice = Console.ReadLine();
+//var userChoice = (Choice)int.Parse(choice);
+
+//var randomizer = new Random();
+//var computerChoice = (Choice)randomizer.Next(1, 4);
+
+//Console.Write($"Computer chose {computerChoice} and {name} chose {userChoice}. ");
+
+//if (computerChoice == Choice.Rock && userChoice == Choice.Scissors
+//    || computerChoice == Choice.Scissors && userChoice == Choice.Paper
+//    || computerChoice == Choice.Paper && userChoice == Choice.Rock)
+//{
+//    Console.WriteLine($"Computer wins.");
+//}
+//else if (computerChoice == userChoice)
+//{
+//    Console.WriteLine("We have a tie.");
+//}
+//else
+//{
+//    Console.WriteLine($"Computer lose");
+//}
+
+//public enum Choice
+//{
+//    Rock = 1,
+//    Scissors,
+//    Paper
+//}
+
+
