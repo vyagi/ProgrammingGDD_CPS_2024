c8c10f0ce22685c3464a0aec00f3793338bb3c56
Marcin <marcin.jagiela@hotmail.com>
2024-11-21 17:45:29 +0100

    Work in progress

--- PracticingTheLanguage/Program.cs
+++ PracticingTheLanguage/Program.cs
@@ -1,7 +1,7 @@
-﻿var myBeautifulCup = new Cup(300);
+﻿var myBeautifulCup = new Container(300);
 myBeautifulCup.Add(100);
-myBeautifulCup.Pour(70);
-Console.WriteLine(myBeautifulCup.LiquidLevel);
+myBeautifulCup.Remove(70);
+Console.WriteLine(myBeautifulCup.ContentLevel);
 
 var coffeeMachine = new CoffeeMachine("Krups 1000", 1500, 1200);
 coffeeMachine.AddGrains(700);
@@ -9,87 +9,36 @@ coffeeMachine.AddWater(1000);
 
 coffeeMachine.MakeEspresso(myBeautifulCup);
 
-Console.WriteLine(myBeautifulCup.LiquidLevel);
+Console.WriteLine(myBeautifulCup.ContentLevel);
 
 public class CoffeeMachine
 {
     private readonly string _modelName;
-    private readonly int _coffeeGrainsCapacity;
-    private readonly int _waterCapacity;
 
-    private int _grainsLevel;
-    private int _waterLevel;
+    private readonly Container _coffeeContainer;
+    private readonly Container _waterContainer;
+
     private int _espressosMade;
 
     public CoffeeMachine(string modelName, int coffeeGrainsCapacity, int waterCapacity)
     {
         _modelName = modelName;
-        _coffeeGrainsCapacity = coffeeGrainsCapacity;
-        _waterCapacity = waterCapacity;
+        _coffeeContainer = new Container(coffeeGrainsCapacity);
+        _waterContainer = new Container(waterCapacity);
     }
 
-    public void AddGrains(int amount)
+    public void AddGrains(int amount) => _coffeeContainer.Add(amount);
+
+    public void AddWater(int amount) => _waterContainer.Add(amount);
+
+    public void MakeEspresso(Container cup)
     {
-        _grainsLevel += amount;
-
-        if (_grainsLevel > _coffeeGrainsCapacity)
-            _grainsLevel = _coffeeGrainsCapacity;
-    }
-
-    public void AddWater(int amount)
-    {
-        _waterLevel += amount;
-
-        if (_waterLevel > _waterCapacity)
-            _waterLevel = _waterCapacity;
-    }
-
-    public void MakeEspresso(Cup cup)
-    {
-        if (_grainsLevel > 50 && _waterLevel > 300)
+        if (_coffeeContainer.ContentLevel > 50 && _waterContainer.ContentLevel > 300)
         {
-            _grainsLevel -= 18;
-            _waterLevel -= 30;
+            _coffeeContainer.Remove(18);
+            _waterContainer.Remove(30);
             _espressosMade++;
             cup.Add(30);
         }
     }
-}
-
-public class Cup
-{
-    private readonly int _capacity;
-	private int _liquidLevel;
-
-	public Cup(int capacity)
-	{
-        if (capacity < 0)
-            throw new ArgumentException(nameof(capacity), "Capacity cannot be negative");
-
-        _capacity = capacity;
-	}
-
-	public void Add(int amount)
-    {
-        if (amount < 0)
-            throw new ArgumentException(nameof(amount), "Amount cannot be negative");
-
-        _liquidLevel += amount;
-
-		if (_liquidLevel > _capacity)
-			_liquidLevel = _capacity;
-    }
-
-	public void Pour(int amount)
-    {
-        if (amount < 0)
-            throw new ArgumentException(nameof(amount), "Amount cannot be negative");
-
-        _liquidLevel -= amount;
-
-        if (_liquidLevel < 0)
-            _liquidLevel = 0;
-    }
-
-    public int LiquidLevel => _liquidLevel;
 }
---
+++ UtilityClasses/Container.cs
@@ -0,0 +1,39 @@
+﻿namespace UtilityClasses;
+
+public class Container
+{
+    private readonly int _capacity;
+	private int _contentLevel;
+
+	public Container(int capacity)
+	{
+        if (capacity < 0)
+            throw new ArgumentException(nameof(capacity), "Capacity cannot be negative");
+
+        _capacity = capacity;
+	}
+
+	public void Add(int amount)
+    {
+        if (amount < 0)
+            throw new ArgumentException(nameof(amount), "Amount cannot be negative");
+
+        _contentLevel += amount;
+
+		if (_contentLevel > _capacity)
+			_contentLevel = _capacity;
+    }
+
+	public void Remove(int amount)
+    {
+        if (amount < 0)
+            throw new ArgumentException(nameof(amount), "Amount cannot be negative");
+
+        _contentLevel -= amount;
+
+        if (_contentLevel < 0)
+            _contentLevel = 0;
+    }
+
+    public int ContentLevel => _contentLevel;
+}
