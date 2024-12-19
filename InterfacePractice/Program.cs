d339d161144323a0f6f5ca0705f95673b4dd7867
Marcin <marcin.jagiela@hotmail.com>
2024-12-19 18:25:51 +0100

    Interface practice done

---
+++ InterfacePractice/Program.cs
@@ -0,0 +1,42 @@
+﻿//var point = new Point(1, 2);
+
+//point.Move(4, 5);
+//point.Move(new Vector(3, 5));
+
+//Console.WriteLine($"{point.X},{point.Y}");
+
+
+//var vector = new Vector(3, 1);
+//Console.WriteLine(vector.Length());
+//Console.WriteLine($"{vector.X},{vector.Y}");
+
+//vector.Scale(2);
+//Console.WriteLine(vector.Length());
+//Console.WriteLine($"{vector.X},{vector.Y}");
+
+//var rectangle = new Rectangle(3, 1);
+//Console.WriteLine($"{rectangle.SideA},{rectangle.SideB}");
+
+//rectangle.Scale(2);
+//Console.WriteLine($"{rectangle.SideA},{rectangle.SideB}");
+
+//var device = new Device();
+//Console.WriteLine(device.IsOn);
+//device.Toggle();
+//Console.WriteLine(device.IsOn);
+//device.Work();
+//device.Work();
+//device.Work();
+//device.Work();
+//Console.WriteLine(device.IsBroken);
+//device.Fix();
+//Console.WriteLine(device.IsBroken);
+//device.Work();
+//device.SwitchOff();
+//Console.WriteLine(device.IsOn);
+//device.Work();
+
+var fr = new FixedRectangle(1, 2, new Point(2, 3));
+fr.Move(-2, -3);
+Console.WriteLine($"{fr.PointOfAttachment.X},{fr.PointOfAttachment.Y}");
+Console.WriteLine(fr.PointOfAttachment.IsInOrigin);
---
+++ InterfacePractice/Types.cs
@@ -0,0 +1,138 @@
+﻿public class Device : ISwitchable, IFixable
+{
+    private int _usages;
+
+    public bool IsBroken { get; private set; }
+
+    public bool IsOn { get; private set; }
+
+    public void SwitchOff() => IsOn = false;
+
+    public void SwitchOn() => IsOn = true;
+
+    public void Toggle() => IsOn = !IsOn;
+
+    public void Work()
+    {
+        if (!IsOn || IsBroken) return;
+     
+        Console.WriteLine("Working..");
+        _usages++;
+
+        if(_usages > 2)
+            IsBroken = true;
+    }
+
+    public void Fix()
+    {
+        _usages = 0;
+        IsBroken = false;
+    }
+}
+
+public class Rectangle : IScalable
+{
+    public double SideA { get; protected set; }
+
+    public double SideB { get; protected set; }
+
+    public Rectangle(double sideA, double sideB) => (SideA, SideB) = (sideA, sideB);
+
+    public void Scale(double coefficient)
+    {
+        SideA *= coefficient; 
+        SideB *= coefficient;
+    }
+}
+
+public class Point : IMoveable
+{
+    public double X { get; private set; }
+
+    public double Y { get; private set; }
+
+    public Point(double x, double y) => (X, Y) = (x, y);
+
+    public bool IsOnAxis => X == 0 || Y == 0;
+
+    public bool IsInOrigin => X == 0 && Y == 0;
+
+    public void Move(double x, double y) 
+    { 
+        X += x;
+        Y += y;
+    }
+
+    public void Move(Vector vector) 
+    {
+        X += vector.X;
+        Y += vector.Y;
+    }
+}
+
+public class Vector : IScalable
+{
+    public double X { get; private set; }
+
+    public double Y { get; private set; }
+
+    public Vector(double x, double y) => (X, Y) = (x, y);
+
+    public double Length() => Math.Sqrt(X * X + Y * Y);
+
+    public void Scale(double coefficient)
+    {
+        X *= coefficient;
+        Y *= coefficient;
+    }
+
+    public bool IsVertical => X == 0;
+
+    public bool IsHorizontal => Y == 0;
+}
+
+public interface IMoveable
+{
+    void Move(double x, double y);
+
+    void Move(Vector vector);
+}
+
+public interface IScalable
+{
+    void Scale(double coefficient);
+}
+
+public interface ISwitchable
+{
+    void SwitchOn();
+    void SwitchOff();
+    void Toggle();
+}
+
+public interface IFixable
+{
+    void Fix();
+
+    bool IsBroken { get; }
+}
+
+public interface IRechargeable
+{
+    void Recharge();
+    double BatteryLevel { get; }
+}
+
+public class FixedRectangle : Rectangle, IMoveable
+{
+    public Point PointOfAttachment { get; protected set; }
+
+    public FixedRectangle(double sideA, double sideB, Point point) : base(sideA, sideB)
+    {
+        PointOfAttachment = point;
+    }
+
+    public void Move(double x, double y) => PointOfAttachment.Move(x, y);
+
+    public void Move(Vector vector) => PointOfAttachment.Move(vector);
+}
